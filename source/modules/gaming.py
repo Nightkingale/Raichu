27e095b740826a90a7712afe8468d503c1248540
NoahAbc12345 <63483138+NoahAbc12345@users.noreply.github.com>
2022-10-17 15:10:26 -0600

    Add improvements to riitag command

--- source/modules/gaming.py
+++ source/modules/gaming.py
@@ -6,14 +6,22 @@ from discord import app_commands
 from discord.ext import commands
 
 
+class RiiTag(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        
+        self.add_item(discord.ui.Button(label='Wii Guide', url="https://wii.guide/"))
+        self.add_item(discord.ui.Button(label='Wii U Hacks Guide', url="https://wiiu.hacks.guide/"))
+
+        
 class Gaming(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    service_group = app_commands.Group(name="service",
+    game_group = app_commands.Group(name="game",
         description="Commands for gaming services.")
 
-    @service_group.command()
+    @game_group.command()
     @app_commands.describe(
         member="The member whose RiiTag should be searched for.")
     async def riitag(self, interaction: discord.Interaction, member: discord.Member = None):
@@ -25,17 +33,20 @@ class Gaming(commands.Cog):
         request = requests.head(tag_link)
 
         if request.headers["content-type"] == "image/png":
-            embed = discord.Embed(title=f"{member.display_name}'s RiiTag", color=0xffff00)
+            embed = discord.Embed(title=f"{member.display_name}'s RiiTag (via RiiConnect24)",
+                description="A showcase of recently played games on Nintendo Wii and Wii U.",
+                url="https://tag.rc24.xyz/", color=0xffff00)
             embed.set_author(name=member.name, icon_url=member.avatar)
-            embed.set_footer(text="This feature is powered by the RiiConnect24 service!")
+            embed.set_footer(text="This feature is powered by an external service!")
             embed.set_image(url=tag_link)
 
-            await interaction.response.send_message("The corresponding RiiTag has been found! "
+            await interaction.response.send_message("A RiiTag has been found! "
                 + "Here's what it looks like.", embed=embed)
         else:
             await interaction.response.send_message("There is no associated RiiTag for this account!"
                 + " To set one up, visit <https://tag.rc24.xyz/> for more information about RiiTags"
-                + " and how they can be used with a modified Wii or Wii U console.")
+                + " and how they can be used with a modified Wii or Wii U console.", 
+                ephemeral=True, view=RiiTag())
 
 
 async def setup(bot: commands.Bot):
