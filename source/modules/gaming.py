ff81988574ca5736f61f1fd7c87acae71efcb980
NoahAbc12345 <63483138+NoahAbc12345@users.noreply.github.com>
2022-10-19 20:53:48 -0600

    Simplify nintendo code with deferring

--- source/modules/gaming.py
+++ source/modules/gaming.py
@@ -17,6 +17,7 @@ class Nintendo(discord.ui.View):
 class Gaming(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.session = aiohttp.ClientSession()
 
     game_group = app_commands.Group(name="game",
         description="Commands for gaming services.")
@@ -31,23 +32,22 @@ class Gaming(commands.Cog):
 
         tag_link = f"https://tag.rc24.xyz/{member.id}/tag.max.png?randomizer={random.random()}"
         
-        async with aiohttp.ClientSession() as session:
-            async with session.get(tag_link) as request:
-                if request.headers["content-type"] == "image/png":
-                    embed = discord.Embed(title=f"{member.display_name}'s RiiTag (via RiiConnect24)",
-                        description="A showcase of recently played games on Nintendo Wii and Wii U.",
-                        url="https://tag.rc24.xyz/", color=0xffff00)
-                    embed.set_author(name=member.name, icon_url=member.avatar)
-                    embed.set_footer(text="This feature is powered by an external service!")
-                    embed.set_image(url=tag_link)
+        async with self.session.get(tag_link) as response:
+            if response.headers["content-type"] == "image/png":
+                await interaction.response.defer()
+                embed = discord.Embed(title=f"{member.display_name}'s RiiTag (via RiiConnect24)",
+                    description="A showcase of recently played games on Nintendo Wii and Wii U.",
+                    url="https://tag.rc24.xyz/", color=0xffff00)
+                embed.set_author(name=member.name, icon_url=member.avatar)
+                embed.set_footer(text="This feature is powered by an external service!")
+                embed.set_image(url=tag_link)
 
-                    await interaction.response.send_message("A RiiTag has been found! "
-                       + "Here's what it looks like.", embed=embed)
-                else:
-                    await interaction.response.send_message("There is no associated RiiTag for this account!"
-                        + " To set one up, visit <https://tag.rc24.xyz/> for more information about RiiTags"
-                        + " and how they can be used with a modified Wii or Wii U console.", 
-                        ephemeral=True, view=RiiTag())
+                await interaction.followup.send("A RiiTag has been found! "
+                    + "Here's what it looks like.", embed=embed)
+            else:
+                await interaction.response.send_message("There is no associated RiiTag for this account!"
+                    + " To set one up, visit <https://tag.rc24.xyz/> for more information about RiiTags"
+                    + " and how they can be used with a modified Wii or Wii U console.", 
                     ephemeral=True, view=Nintendo())
 
 
