c0302e4bbd980a2c79ba0f783f9bb4c5e43f188f
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2023-06-27 15:05:39 -0600

    Hide branch/commit if not found

--- source/modules/inform.py
+++ source/modules/inform.py
@@ -1,5 +1,5 @@
 import discord
-import os
+import subprocess
 
 from discord import app_commands
 from discord.ext import commands
@@ -17,39 +17,28 @@ class Inform(commands.Cog):
     @inform_group.command()
     async def build(self, interaction: discord.Interaction):
         "Shows information regarding the bot."
-        commit = os.environ.get("COMMIT_SHA", "Unknown")
-        branch = os.environ.get("COMMIT_BRANCH", "Unknown")
-
-        if commit == "Unknown":
-            try:
-                # Fetches the commit hash from the git repository.
-                commit = check_output(
-                    ["git", "rev-parse", "HEAD"]).decode("ascii")[:-1]
-            except:
-                # Defaults when the git repository is not found.
-                commit = "0000000"
-
-        if branch == "Unknown":
-            try:
-                # Fetches the branch name from the git repository.
-                branch = check_output(
-                    ["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode()[:-1]
-            except:
-                # Defaults when the git repository is not found.
-                branch = "master"
-
         embed = discord.Embed(title="Raichu", url="https://github.com/Nightkingale/Raichu",
             description="A Discord utility bot for Nincord.", color=0xffff00)
         embed.set_author(name="Nightkingale", url="https://nightkingale.com",
             icon_url="https://avatars.githubusercontent.com/u/63483138?v=4")
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/"
             + "983846918683770941/7f2ad37cee31d9599ae51a1d3082fb56.png?size=256")
-        embed.add_field(name="Branch", value="`" + branch + "`", inline=True)
 
-        if commit != "Unknown":
+        try:
+            # Fetches the commit hash from the git repository.
+            commit = check_output(
+                ["git", "rev-parse", "HEAD"]).decode("ascii")[:-1]
             embed.add_field(name="Commit", value="`" + commit[0:7] + "`", inline=True)
-        else:
-            embed.add_field(name="Commit", value="`" + commit + "`", inline=True)
+        except subprocess.CalledProcessError:
+            pass
+
+        try:
+            # Fetches the branch name from the git repository.
+            branch = check_output(
+                ["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode()[:-1]
+            embed.add_field(name="Branch", value="`" + branch + "`", inline=True)
+        except subprocess.CalledProcessError:
+            pass
 
         embed.set_footer(text="Check out my source code on GitHub!")
         await interaction.response.send_message("Here's some information about me!",
