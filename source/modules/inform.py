5a88555f6ed3641ce4c8e8ca92744c79ec8fd629
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2023-06-27 13:36:38 -0600

    Split up commit and branch trees

--- source/modules/inform.py
+++ source/modules/inform.py
@@ -20,17 +20,23 @@ class Inform(commands.Cog):
         commit = os.environ.get("COMMIT_SHA", "Unknown")
         branch = os.environ.get("COMMIT_BRANCH", "Unknown")
 
-        if commit == "Unknown" or branch == "Unknown":
+        if commit == "Unknown":
             try:
-                # Fetches the commit and branch names from the git repository.
+                # Fetches the commit hash from the git repository.
                 commit = check_output(
                     ["git", "rev-parse", "HEAD"]).decode("ascii")[:-1]
+            except:
+                # Defaults when the git repository is not found.
+                commit = "0000000"
+
+        if branch == "Unknown":
+            try:
+                # Fetches the branch name from the git repository.
                 branch = check_output(
                     ["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode()[:-1]
             except:
                 # Defaults when the git repository is not found.
-                commit = "Unknown"
-                branch = "Unknown"
+                branch = "master"
 
         embed = discord.Embed(title="Raichu", url="https://github.com/Nightkingale/Raichu",
             description="A Discord utility bot for Nincord.", color=0xffff00)
@@ -38,12 +44,12 @@ class Inform(commands.Cog):
             icon_url="https://avatars.githubusercontent.com/u/63483138?v=4")
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/"
             + "983846918683770941/7f2ad37cee31d9599ae51a1d3082fb56.png?size=256")
-        embed.add_field(name="Branch", value=branch, inline=True)
+        embed.add_field(name="Branch", value="`" + branch + "`", inline=True)
 
         if commit != "Unknown":
-            embed.add_field(name="Commit", value=commit[0:7], inline=True)
+            embed.add_field(name="Commit", value="`" + commit[0:7] + "`", inline=True)
         else:
-            embed.add_field(name="Commit", value=commit, inline=True)
+            embed.add_field(name="Commit", value="`" + commit + "`", inline=True)
 
         embed.set_footer(text="Check out my source code on GitHub!")
         await interaction.response.send_message("Here's some information about me!",
