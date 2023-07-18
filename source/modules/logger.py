424031d37581acaa8ba51f2da74870b1b2a6d67a
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2023-07-18 10:17:21 -0600

    Change the logging format

--- source/modules/logger.py
+++ source/modules/logger.py
@@ -4,8 +4,7 @@ def create_logger(logger_name):
     # Set up the logger.
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s %(levelname)-8s %(name)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        format="[%(levelname)s] %(name)s: %(message)s",
     )
 
     # Create a logger specific to the specified name
