84cb09dbb7f6650226d2e618517219e24ef5f847
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2023-12-30 00:22:22 -0700

    Expand on giveaway system

--- source/modules/gifting.py
+++ source/modules/gifting.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import pymongo
+import random
 
 from discord import app_commands
 from discord.ext import commands
@@ -36,25 +37,8 @@ class Gifting(commands.Cog):
 
 
     @giveaway_group.command()
-    @app_commands.describe(
-        giveaway="The name of the giveaway to join or leave.")
-    async def toggle(self, interaction: discord.Interaction, giveaway: str):
-        "Add or remove yourself from a giveaway."
-        # Check if the giveaway exists and is not archived.
-        if not database.list_collection_names().__contains__(giveaway) or database[giveaway].name.endswith("[archive]"):
-            await interaction.response.send_message("That giveaway does not exist! Make sure you typed the name exactly as announced.",
                 ephemeral=True)
             return
-        # Check if the user has already entered the giveaway.
-        if database[giveaway].find_one({"_id": interaction.user.id}):
-            database[giveaway].delete_one({"_id": interaction.user.id})
-            await interaction.response.send_message(f"You have left the **{giveaway}** giveaway. We're sorry to see you go!", ephemeral=True)
-            self.logger.info(f"{interaction.user.name} has left the giveaway.")
-            return
-        # Add the user to the giveaway.
-        database[giveaway].insert_one({"_id": interaction.user.id})
-        await interaction.response.send_message(f"You have entered the **{giveaway}** giveaway! We wish you the best of luck!", ephemeral=True)
-        self.logger.info(f"{interaction.user.name} has joined the giveaway.")
 
 
     @giveaway_group.command()
@@ -62,7 +46,8 @@ class Gifting(commands.Cog):
     async def decide(self, interaction: discord.Interaction, giveaway: str, amount: int):
         "Decide a winner for a specified giveaway."
         # Check if the giveaway exists.
-        if not database.list_collection_names().__contains__(giveaway) or database[giveaway].name.endswith("[archive]"):
+        giveaway_entry = database["Ongoing"].find_one({"_id": giveaway})
+        if not giveaway_entry:
             await interaction.response.send_message("That giveaway does not exist! Make sure you typed the name exactly as announced.",
                 ephemeral=True)
             return
@@ -71,21 +56,25 @@ class Gifting(commands.Cog):
             await interaction.response.send_message("You must choose at least one winner for the giveaway!",
                 ephemeral=True)
             return
-        if amount > database[giveaway].estimated_document_count():
-            participant_count = database[giveaway].estimated_document_count()
+        if amount > len(giveaway_entry["users"]):
+            participant_count = len(giveaway_entry["users"])
             await interaction.response.send_message(
                 f"You cannot choose more winners than there are participants (currently {participant_count}).",
                 ephemeral=True
             )
             return
         # Choose winner(s), making sure not to pick the same person twice.
-        winner = database[giveaway].aggregate([{"$sample": {"size": amount}}])
+        winners = random.sample(giveaway_entry["users"], amount)
         # Send a message to the channel congratulating and mentioning the winner(s).
-        winner_ids = [f'<@{document["_id"]}>' for document in winner]
+        winner_ids = [f'<@{winner_id}>' for winner_id in winners]
         # Create an embed for the giveaway winner(s).
         embed = discord.Embed(title=f"Winner{'s' if amount > 1 else ''} of the {giveaway} Giveaway",
             description=f"Congratulations to the winner{'s' if amount > 1 else ''} of the giveaway!", color=0xffff00)
-        embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
+        try:
+            host = await self.bot.fetch_user(giveaway_entry["host"])
+        except discord.NotFound:
+            host = interaction.user
+        embed.set_author(name=host.display_name, icon_url=host.display_avatar)
         embed.set_thumbnail(url="https://media.tenor.com/3fBEgjA2Y6IAAAAi/giveaway-alert-giveaway.gif")
         embed.add_field(name=f"Winner{'s' if amount > 1 else ''} ({amount} total)", value="\n".join(winner_ids))
         embed.set_footer(text="A notification will be sent. Please contact the host to claim your gift.")
@@ -95,7 +84,7 @@ class Gifting(commands.Cog):
             if winner:
                 await winner.send(
                     f"Congratulations! You have won the **{giveaway}** giveaway on the {interaction.guild.name} server!\n\n"
-                    f"Please contact the host ({interaction.user.mention}) or a member of the {interaction.guild.name} staff team "
+                    f"Please contact the host ({host.mention}) or a member of the {interaction.guild.name} staff team "
                     f"in order to proceed and claim your prize. If the host or a staff member has already reached out to you, "
                     f"please disregard this message. Thank you so much for your participation!",
                     embed=embed
@@ -105,8 +94,36 @@ class Gifting(commands.Cog):
         await interaction.response.send_message(f"The results are in for the **{giveaway}** giveaway! Congratulations!",
             embed=embed)
         self.logger.info(f"{interaction.user.name} has decided for the {giveaway} giveaway.")
-        # Close the giveaway.
-        database[giveaway].rename(f"{giveaway} [archive]")
+        # Move the giveaway to Archived from Ongoing.
+        database["Archived"].replace_one({"_id": giveaway}, giveaway_entry, upsert=True)
+        database["Ongoing"].delete_one({"_id": giveaway})
+
+
+    @giveaway_group.command()
+    @app_commands.describe(
+        giveaway="The name of the giveaway to join or leave.")
+    async def enter(self, interaction: discord.Interaction, giveaway: str):
+        "Add or remove yourself from a giveaway."
+        # Check if the giveaway exists and is not archived.
+        giveaway_entry = database["Ongoing"].find_one({"_id": giveaway})
+        if not giveaway_entry:
+            await interaction.response.send_message("That giveaway does not exist! Make sure you typed the name exactly as announced.",
+                ephemeral=True)
+            return
+        # Initialize the users field if it doesn't exist.
+        if "users" not in giveaway_entry:
+            database["Ongoing"].update_one({"_id": giveaway}, {"$set": {"users": []}})
+            giveaway_entry["users"] = []
+        # Check if the user has already entered the giveaway.
+        if interaction.user.id in giveaway_entry["users"]:
+            database["Ongoing"].update_one({"_id": giveaway}, {"$pull": {"users": interaction.user.id}})
+            await interaction.response.send_message(f"You have left the **{giveaway}** giveaway. We're sorry to see you go!", ephemeral=True)
+            self.logger.info(f"{interaction.user.name} has left the {giveaway} giveaway.")
+            return
+        # Add the user to the giveaway.
+        database["Ongoing"].update_one({"_id": giveaway}, {"$push": {"users": interaction.user.id}})
+        await interaction.response.send_message(f"You have entered the **{giveaway}** giveaway! We wish you the best of luck!", ephemeral=True)
+        self.logger.info(f"{interaction.user.name} has joined the {giveaway} giveaway.")
 
 
 async def setup(bot: commands.Bot):
