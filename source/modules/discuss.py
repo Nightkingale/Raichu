b5e4b2aa85ce1c899f1e9e034c3984d369b04f08
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2024-01-07 18:09:04 -0700

    Only respond if specifically mentioned

--- source/modules/discuss.py
+++ source/modules/discuss.py
@@ -62,7 +62,7 @@ class Discuss(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if self.bot.user.mentioned_in(message) and message.author != self.bot.user:
+        if self.bot.user.mentioned_in(message) and message.author != self.bot.user and not message.mention_everyone:
             # Get the names of the bot, user, and server.
             bot_name = discord.utils.get(message.guild.members, id=self.bot.user.id).display_name
             user_name = message.author.display_name
