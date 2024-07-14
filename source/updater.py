962ec933cacaf992149e80cc582adef578170412
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2024-07-14 12:08:55 -0600

    Make the updater code less "nested"

--- source/updater.py
+++ source/updater.py
@@ -20,13 +20,15 @@ class Updater(commands.Cog):
     # Look for new commits in the GitHub logs channel and automatically update.
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.id == config["channels"]["#github-logs"]:
-            for embed in message.embeds:
-                if "Raichu" in embed.title and "new commit" in embed.title:
-                    if os.name == "posix":
-                        # Run the updater service in Area Zero. If this isn't Area Zero, the service won't exist.
-                        self.logger.info("A new commit was detected. An automatic update will be performed.")
-                        os.system("sudo systemctl start raichu_update.service")
+        if message.channel.id != config["channels"]["#github-logs"]:
+            return # Not the GitHub logs channel.
+
+        for embed in message.embeds:
+            if "Raichu" in embed.title and "new commit" in embed.title:
+                if os.name == "posix":
+                    # Run the updater service in Area Zero. If this isn't Area Zero, the service won't exist.
+                    self.logger.info("A new commit was detected. An automatic update will be performed.")
+                    os.system("sudo systemctl start raichu_update.service")
 
 
 async def setup(bot: commands.Bot):
