d5080ade7cb2226594951ba2f3c5f0680858a970
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2025-04-20 19:49:55 -0600

    Temporarily disable RiiTag command

--- source/games.py
+++ source/games.py
@@ -15,35 +15,38 @@ class Games(commands.Cog):
         self.logger = create_logger(self.__class__.__name__)
 
 
-    @app_commands.command()
-    @app_commands.describe(
-        member="The member whose RiiTag should be searched for.")
-    async def riitag(self, interaction: discord.Interaction, member: discord.Member = None):
-        "Shows a RiiTag from the RiiConnect24 service."
-        if member == None:
-            member = interaction.user
+    # RiiTag no longer exists at this time.
+    # Once it's replaced with LinkTag, we can update this code accordingly.
 
-        # Discord cache is wonky, so we need to add a randomizer to the URL.
-        tag_link = f"https://tag.rc24.xyz/{member.id}/tag.max.png?randomizer={random.random()}"
-
-        # Check if the member has a RiiTag.
-        await interaction.response.defer()
-        async with self.session.get(tag_link) as response:
-            # Check if the response from the site actually contains an image.
-            if response.status == 200 and response.headers["content-type"] == "image/png":
-                self.logger.info(f"A RiiTag was fetched for {member.display_name} at {tag_link}.")
-                embed = discord.Embed(title=f"{member.display_name}'s RiiTag (via RiiConnect24)",
-                    description="A showcase of recently played games on Nintendo consoles.",
-                    url="https://tag.rc24.xyz/", color=0xffff00)
-                # embed.set_author(name=member.name, icon_url=member.avatar)
-                embed.set_footer(text="This feature is powered by an external service!")
-                embed.set_image(url=tag_link)
-                # Send the embed to the channel that the command was used in.
-                await interaction.followup.send("A RiiTag was successfully found."
-                    + " Here's what it looks like!", embed=embed)
-            else:
-                await interaction.followup.send("There is no associated RiiTag for this account!"
-                    + " Visit <https://tag.rc24.xyz> for more information.")
+    # @app_commands.command()
+    # @app_commands.describe(
+    #     member="The member whose RiiTag should be searched for.")
+    # async def riitag(self, interaction: discord.Interaction, member: discord.Member = None):
+    #     "Shows a RiiTag from the RiiConnect24 service."
+    #     if member == None:
+    #         member = interaction.user
+    #
+    #     # Discord cache is wonky, so we need to add a randomizer to the URL.
+    #     tag_link = f"https://tag.rc24.xyz/{member.id}/tag.max.png?randomizer={random.random()}"
+    #
+    #     # Check if the member has a RiiTag.
+    #     await interaction.response.defer()
+    #     async with self.session.get(tag_link) as response:
+    #         # Check if the response from the site actually contains an image.
+    #         if response.status == 200 and response.headers["content-type"] == "image/png":
+    #             self.logger.info(f"A RiiTag was fetched for {member.display_name} at {tag_link}.")
+    #             embed = discord.Embed(title=f"{member.display_name}'s RiiTag (via RiiConnect24)",
+    #                 description="A showcase of recently played games on Nintendo consoles.",
+    #                 url="https://tag.rc24.xyz/", color=0xffff00)
+    #             # embed.set_author(name=member.name, icon_url=member.avatar)
+    #             embed.set_footer(text="This feature is powered by an external service!")
+    #             embed.set_image(url=tag_link)
+    #             # Send the embed to the channel that the command was used in.
+    #             await interaction.followup.send("A RiiTag was successfully found."
+    #                 + " Here's what it looks like!", embed=embed)
+    #         else:
+    #             await interaction.followup.send("There is no associated RiiTag for this account!"
+    #                 + " Visit <https://tag.rc24.xyz> for more information.")
                 
 
     @app_commands.command()
